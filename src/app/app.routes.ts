e74d5a9859874e796606fa4fa7c48e08bf16cc49
Diego Ramirez <dialrasa2@gmail.com>
2024-12-21 15:52:52 -0500

    refactor: add main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'auth',
+		loadComponent: () => import('./pages/auth/auth.component').then(c => c.AuthComponent),
+	},
+	{
+		path: 'dashboard',
+		loadComponent: () => import('./pages/dashboard/dashboard.component').then(c => c.DashboardComponent),
+	},
+	{ path: '',   redirectTo: '/auth', pathMatch: 'full' },
+];

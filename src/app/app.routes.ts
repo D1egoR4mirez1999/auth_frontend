ab98e0337285d169aa310faf94373f73b0be51a6
Diego Ramirez <dialrasa2@gmail.com>
2024-12-21 16:00:34 -0500

    refactor: adjust routes to see sign-in and sign-up components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,20 @@ export const routes: Routes = [
 	{
 		path: 'auth',
 		loadComponent: () => import('./pages/auth/auth.component').then(c => c.AuthComponent),
+		children: [
+			{
+				path: 'sign-in',
+				loadComponent: () => import('./pages/auth/components/sign-in/sign-in.component').then(c => c.SignInComponent),
+			},
+			{
+				path: 'sign-up',
+				loadComponent: () => import('./pages/auth/components/sign-up/sign-up.component').then(c => c.SignUpComponent),
+			}
+		]
 	},
 	{
 		path: 'dashboard',
 		loadComponent: () => import('./pages/dashboard/dashboard.component').then(c => c.DashboardComponent),
 	},
-	{ path: '',   redirectTo: '/auth', pathMatch: 'full' },
+	{ path: '',   redirectTo: '/auth/sign-in', pathMatch: 'full' },
 ];

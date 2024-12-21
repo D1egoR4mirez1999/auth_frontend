70d6e617fb2a1d9ca21815e3704aa2d898d5da46
Diego Ramirez <dialrasa2@gmail.com>
2024-12-21 16:54:47 -0500

    refactor: add reactive form in sign-in.component

--- src/app/pages/auth/components/sign-in/sign-in.component.ts
+++ src/app/pages/auth/components/sign-in/sign-in.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  formBuilder = inject(FormBuilder);
+  signInForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
 
+  signIn(): void {
+    console.log(this.signInForm.value);
+  }
 }

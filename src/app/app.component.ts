ff8a806bdd424bc293370909d64ec6eec72aa08d
Diego Ramirez <dialrasa2@gmail.com>
2024-12-24 14:45:07 -0500

    refactor: adjust app.component to get the refresh token and handle the user authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+
+import { AuthService } from './pages/auth/services/auth.service';
+import { authState } from './pages/auth/enums/auth-state.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  authService = inject(AuthService);
+  router = inject(Router);
+  isCheckingState = computed<boolean>(() => {
+    return this.authService.authState() === authState.CHECKING;
+  });
+
+  ngOnInit(): void {
+    this.authService.refreshToken().subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        this.router.navigate(['/auth/sign-in']);
+      },
+    });
+  }
+}

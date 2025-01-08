6139e648e8b4a3740894528ee35922215195ea44
Diego Ramirez <dialrasa2@gmail.com>
2025-01-08 15:47:27 -0500

    feat: add sign-up template with reactive form

--- src/app/pages/auth/components/sign-up/sign-up.component.ts
+++ src/app/pages/auth/components/sign-up/sign-up.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  formBuilder = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  signUpForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    name: ['', [Validators.required]],
+  });
+
+  signUp(): void {
+    const params = this.signUpForm.value as { email: string, password: string, name: string };
+  }
 }
